db9b29ed4d0f26b153978faf5a6666746e499d63
Loránd Kele <lorand996@gmail.com>
2022-11-22 13:03:20 +0100

    bugos rendorkocsi

---
+++ PoliceCar.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class PoliceCar : Node2D
+{
+   
+    private Node2D Car;
+    private KinematicBody2D CarBody;
+
+    private Vector2 target;
+
+    private KinematicBody2D PoliceBody;
+
+    public override void _Ready()
+    {
+        PoliceBody = GetNode("KinematicBody2D") as KinematicBody2D;
+        Car = GetNode("/root/Node2D/Car") as Node2D; 
+        CarBody = GetNode("/root/Node2D/Car/KinematicBody2D") as KinematicBody2D; 
+    }
+
+
+  public override void _Process(float delta)
+  {
+      target = Position.DirectionTo((Car.Position + CarBody.Position ) - PoliceBody.Position);
+      //this.LookAt(Car.Position + CarBody.Position);
+      PoliceBody.MoveAndSlide(target * delta);
+  }
+}
