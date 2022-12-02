5e51dfb5c6a7470a6e211ee88d6fb2ed24c85190
Loránd Kele <lorand996@gmail.com>
2022-12-02 09:37:56 +0100

    asd

--- PoliceCar.cs
+++ PoliceCar.cs
@@ -3,24 +3,26 @@ using System;
 
 public class PoliceCar : Node2D
 {
-   
-    private Node2D Car;
-    private KinematicBody2D CarBody;
 
-    private Vector2 target;
+    private Vector2 direction;
+    private Node2D Car;
+    private Sprite CarBody;
 
     private KinematicBody2D PoliceBody;
 
+
     public override void _Ready()
     {
+        direction = new Vector2(0, 0);
         PoliceBody = GetNode("KinematicBody2D") as KinematicBody2D;
-        Car = GetNode("/root/Node2D/Car") as Node2D; 
-        CarBody = GetNode("/root/Node2D/Car/KinematicBody2D") as KinematicBody2D; 
+        Car = GetNode("/root/Node2D/Car") as Node2D;
+        CarBody = GetNode("/root/Node2D/Car/KinematicBody2D/Sprite") as Sprite;
     }
 
 
-     public override void _Process(float delta)
-    {   
-      this.LookAt(CarBody.Position);
+    public override void _Process(float delta)
+    {
+        this.LookAt(Car.Position + CarBody.Position);
+        
     }
 }
