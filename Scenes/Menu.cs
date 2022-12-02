23aa6516cf97d969817de4448bb046788d0e55bb
Kancsal Máté <71323998+ahelyikemeny@users.noreply.github.com>
2022-12-02 13:02:03 +0100

    menu

---
+++ Scenes/Menu.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Menu : Control
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+ public void _on_Start_pressed(){
+     GetTree().ChangeScene("res://Scenes/Game.tscn");
+ }
+ public void _on_Exit_pressed(){
+     GetTree().Quit();
+ }
+}
